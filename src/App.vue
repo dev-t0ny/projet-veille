95b8572b720f403ab2c21704499ec9f589f43af7
dev-t0ny <antoineboudreau@Outlook.com>
2025-10-21 23:39:31 -0400

    fix - déploiement

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-xl">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-xl text-white text-center">You did it!</h1>
 </template>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projet-veille/',
   plugins: [
     vue(),
     vueDevTools(),
